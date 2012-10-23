d57a677d8d50e8c4f27778eb8c7297d7bfa56095
Nathan Amick <github@nathanamick.com>
2012-10-23 16:06:40 +0000

    Add seed cap task

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,3 +50,11 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
+
+namespace :deploy do
+  desc "reload the database with seed data"
+  task :seed do
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
+  end
+end
+
